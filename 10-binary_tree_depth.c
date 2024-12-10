e97291e03d21223591f4370f7d63eb042a1e4d29
Mollydayne <cl.w.perez@gmail.com>
2024-12-10 11:24:39 +0100

    Adding task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,10 +14,18 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	size_t nodeCounter = 0;
 
-    
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+while (tree->parent != NULL)
+{
+	nodeCounter++;
+	tree = tree->parent;
+}
+
+return (nodeCounter);
 }
